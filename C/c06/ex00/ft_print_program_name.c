a3b5a5ed32fe7ed50bcb24448f2a760c23e4dce0
SE <se@192.168.1.235>
2026-08-10 22:13:22 +0100

    c06 ex00

---
+++ C/c06/ex00/ft_print_program_name.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+
+void ft_putchar(char c){
+	write(1,&c,1);
+}
+
+int main(int argc,char * argv[]){
+	int i = 0;
+	(void)argc;
+	while(argv[0][i] != '\0'){
+		ft_putchar(argv[0][i]);
+		i++;
+	}
+	ft_putchar('\n');
+	return 0;
+}
