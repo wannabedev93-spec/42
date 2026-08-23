98ceea2f1269303be00f17320c9a5ab912065b0b
SE <se@MacBook-Pro-von-dev.local>
2026-08-23 01:33:44 +0100

    c08 ex01

---
+++ C/c08/ex01/ft_boolean.h
@@ -0,0 +1,14 @@
+#ifndef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+#include<stdlib.h>
+#define EVEN(nbr) !((nbr) % 2)
+#define TRUE 1
+#define FALSE 0
+#define ODD_MSG "I have an odd number of arguments."
+#define EVEN_MSG "I have an even number of arguments."
+#define SUCCESS 0 
+typedef int  t_bool;
+
+
+
+#endif
